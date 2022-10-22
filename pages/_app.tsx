45647fbabb42907a8601cbb163211db47b37c1f2
Joy Lee <joylee.dev@gmail.com>
2022-10-22 23:55:05 +0900

    💄 add background image layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import styles from '../styles/Layout.module.css'
+import Image from 'next/image'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className={styles.bg}>
+      <div className={styles.innerbox}></div>
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
